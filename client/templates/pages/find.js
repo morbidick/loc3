76fcb2bc8446af6d510cc231d48d0cfc039a1e4d
lennart <uaifr@student.kit.edu>
2014-12-03 18:43:26 +0100

    added and implemented advanced search for our item db

--- client/templates/pages/find.js
+++ client/templates/pages/find.js
@@ -1,28 +1,79 @@
+Template.searchForm.created = function () {
+  Session.set("query", {"name": "", "team": "", "vendor": "", "locations": []});
+}
+
 Template.searchForm.events({
-  "submit .search": function (event) {
-    var text = event.target.text.value;
-    Session.set("search", text);
-    event.target.text.value = "";
+  "submit form": function (event) {
+  	var name, team, vendor, locations, query;
+  	name = $( '#queryName1' ).val();
+    team = $( '#queryTeam1' ).val();
+  	vendor = $( '#queryVendor1' ).val();
+
+  	locations = [];
+  	if ( $( '#queryLocations1' ).prop( 'checked' ) ) {
+	  	locations.push("world");		
+  	}
+  	if ( $( '#queryLocations2' ).prop( 'checked' ) ) {
+  		locations.push("halleh");
+  	}
+  	if ( $( '#queryLocations3' ).prop( 'checked' ) ) {
+	  	locations.push("congress");  		
+  	}
+  	if ( $( '#queryLocations4' ).prop( 'checked' ) ) {
+  		locations.push("transport");
+  	}
+    // Meteor.call("logCommand", {"name": name, "team": team, "vendor": vendor, "locations": locations});
+    Session.set("query", {"name": name, "team": team, "vendor": vendor, "locations": locations});
     return false;
   }
 });
 
 Template.searchResults.helpers({
 	"results": function() {
-		var searchTerm, results;
-		searchTerm = Session.get("search");
-		if (searchTerm !== undefined && searchTerm !== null && searchTerm !== "") {
-			searchTerm = ".*" + searchTerm + ".*";
-			results = Items.find({name: {$regex: searchTerm}}, {limit: 100});
-			Session.set("resultQty", results.fetch().length);
-			return results;
-		}
+		var query, name, team, vendor, locations, results, active;
+		query = Session.get("query");
+    name = "";
+    var modq = {};
+    if (query["name"] && query["name"] !== "") {
+      name = ".*" + name + query["name"] + ".*";
+      modq["name"] = {"$regex": name};
+      active = true;
+    }
+    team = "";
+    if (query["team"] && query["team"] !== "") {
+      team = ".*" + team + query["team"] + ".*";
+      modq["team"] = {"$regex": team};
+      active = true;
+    }
+    vendor = "";
+    if (query["vendor"] && query["vendor"] !== "") {
+      vendor = ".*" + vendor + query["vendor"] + ".*";
+      modq["team"] = {"$regex": team};
+      active = true;
+    }    
+    locations = query["locations"];
+    if (locations.length > 0) {
+      modq["location.type"] = {"$in": locations}
+      active = true;
+    }
+
+    Meteor.call("logCommand", modq);
+
+    results = Items.find(modq, {limit: 20});
+    Session.set("resultQty", results.count());
+
+    return results;
 	},
 
 	"qty": function() {
 		var resultQty = Session.get("resultQty");
 		if((typeof resultQty) === "number" && resultQty > 0) {
-			return resultQty + " results for: \"" + Session.get("search") + "\"";
+      if (resultQty < 101) {
+  			return resultQty + " results";
+      }
+      else {
+        return resultQty + " results, only 100 are shown"
+      }
 		}
 		else {
 			return "no results";
