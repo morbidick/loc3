9ecc0495d7ffc13a3a216a0d03a657ce2d34178b
lennart <uaifr@student.kit.edu>
2014-12-19 23:18:45 +0100

    find testing

--- client/templates/pages/find.js
+++ client/templates/pages/find.js
@@ -1,10 +1,5 @@
 Template.searchForm.created = function () {
-  // TODO figure out setDefaults behavior and why it seems to be confusing if used here 
-  // this.query = new ReactiveVar();
-  // this.query.set({});
-  if(typeof Session.get("searchQuery") === "object") {
-    Session.set("searchQuery", {});
-  }
+  Session.set("searchQuery", {});
 }
 
 // Fulltext search events
@@ -15,10 +10,24 @@ Template.searchForm.events({
   	var name, team, vendor, locations, query;
   	// basic fields
     query = {
-      name: $( '#queryName1' ).val(),
-      team: $( '#queryTeam1' ).val(),
-      vendor: $( '#queryVendor1' ).val()
+      name: template.$( '#queryName' ).val(),
+      team: template.$( '#queryTeam' ).val(),
+      vendor: template.$( '#queryVendor' ).val(),
+      shipper: template.$( '#queryShipper' ).val()
     };
+    var field;
+    for (field in query) {
+      query[field] = ".*" + query[field] + ".*";
+      query[field] = new RegExp(query[field], "i");      
+    }
+
+    // check for correct location if (and only if) at least one location was set
+    // locations = query["locations"];
+    // if (locations.length > 0) {
+    //   modq["location.type"] = {"$in": locations}
+    //   active = true;
+    // }
+    // console.log(query);
     Session.set("searchQuery", query)
     // prevent default handler
     return false;
@@ -27,48 +36,28 @@ Template.searchForm.events({
 
 // Fulltext search results helpers
 Template.searchResults.helpers({
-	"results": function() {
-		var query, name, team, vendor, locations, results, active;
-		query = Session.get("searchQuery");
-    name = "";
-    var modq = {};
-    return [];
-    // check we are active, i.e. a meaningfull search was submitted
-    if (query.name && query.name !== "") {
-      // build ourselves a regex for case insensitive fulltext
-      name = ".*" + name + query.name + ".*";
-      name = new RegExp(name, "i");
-      modq["name"] = {"$regex": name};
-      active = true;
-    }
-    team = "";
-    if (query["team"] && query["team"] !== "") {
-      team = ".*" + team + query["team"] + ".*";
-      team = new RegExp(team, "i");
-      modq["team"] = {"$regex": team};
-      active = true;
-    }
-    vendor = "";
-    if (query["vendor"] && query["vendor"] !== "") {
-      vendor = ".*" + vendor + query["vendor"] + ".*";
-      vendor = new RegExp(vendor, "i");
-      modq["vendor"] = {"$regex": vendor};
-      active = true;
-    }
-    // check for correct location if (and only if) at least one location was set
-    locations = query["locations"];
-    if (locations.length > 0) {
-      modq["location.type"] = {"$in": locations}
-      active = true;
-    }
-    // TODO push database action to server
-    if(active) {
-      results = Items.find(modq);
-      Session.set("resultQty", results.count());
-    }
+  "results": function() {
+    var query = Session.get("searchQuery");
+    var results = {};
+    results = Items.find(query);
+    Session.set("resultQty", results.count());
     return results;
 	},
-
+  "fields": function () {
+    console.log(Session.get("searchQuery"));
+    var fromdb = Items.findOne(Session.get("searchQuery"));
+    console.log(fromdb);
+    var keys = Object.keys(fromdb);
+    console.log(keys);
+    return keys;
+  },
+  "values": function (name) {
+    console.log(Session.get("searchQuery"));
+    var fromdb = Items.findOne(Session.get("searchQuery"));
+    console.log(fromdb);
+    var values = $.map(sublocations, function(el) { return el; });
+    return values;
+  },
   // Checks for number of results
 	"qty": function() {
 		var resultQty = Session.get("resultQty");
