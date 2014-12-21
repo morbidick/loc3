a78ffd1bf2bab68684d01a8589eff64f8c446752
lennart <uaifr@student.kit.edu>
2014-12-21 18:42:44 +0100

    ups

--- client/templates/pages/start.js
+++ client/templates/pages/start.js
@@ -1,9 +1,3 @@
 Template.startPage.helpers({
   "info_url": Meteor.settings.public.info_url
 })
-
-Template.startPage.events({
-	"click .clickme": function () {
-		Meteor.call("correctUsers");
-	}
-});
