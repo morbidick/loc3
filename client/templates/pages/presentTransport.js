e4def02df5fb653f81835ed7430e757be34814a6
lennart <uaifr@student.kit.edu>
2014-12-25 00:04:06 +0100

    transport presentation fixes

--- client/templates/pages/presentTransport.js
+++ client/templates/pages/presentTransport.js
@@ -1,7 +1,7 @@
 Template.presentTransport.helpers({
 	"fromdb": function () {
-		console.log(name);
 		var fromdb = Transports.findOne({"_id": this.name});
+		console.log(this.name);
 		console.log(fromdb);
 		return fromdb;
 	}
