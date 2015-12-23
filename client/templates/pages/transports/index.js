642efcbdc3ff19c61173ea9f5e32ea8401bcf39c
morbidick <mail@morbi-happens.de>
2015-12-23 15:15:33 +0100

    more glitzer for transport page

--- client/templates/pages/transports/index.js
+++ client/templates/pages/transports/index.js
@@ -1,5 +1,54 @@
 Template.transportsPage.helpers({
-	"transports": function() {
-		return Transports.find();
+	tableSettings: function () {
+		return {
+			collection: Transports,
+			rowsPerPage: 50,
+			showFilter: true,
+			showColumnToggles: true,
+			rowClass: function(item) { if (item.is_ccc) {return 'warning'}; },
+			fields: [{
+				key: 'name',
+				label: "Name",
+				hideToggle: true,
+				sortOrder: 2,
+			}, {
+				key: 'vendor',
+				label: "Vendor",
+			}, {
+				key: 'home',
+				label: "Home",
+				hidden: true,
+			}, {
+				key: 'shipper',
+				label: "Shipper",
+				hidden: true,
+			}, {
+				key: 'arrival',
+				label: 'Arrival',
+				fn: function (value, object) { return (value)? moment(value).format('DD.MM.YYYY HH:mm') : ""; },
+				sortByValue: true,
+				sortOrder: 0,
+			}, {
+				key: 'departure',
+				label: 'Departure',
+				fn: function (value, object) { return (value)? moment(value).format('DD.MM.YYYY HH:mm') : ""; },
+				sortByValue: true,
+				sortOrder: 1,
+			}, {
+				key: 'comment',
+				label: "Comment",
+				hidden: true,
+			}, {
+				key: 'file_ids',
+				label: "Files",
+				tmpl: Template.transportsTableFiles,
+			}, {
+				key: "modify",
+				label: "",
+				hideToggle: true,
+				tmpl: Template.transportsTableModify,
+				sortable: false,
+			}]
+		};
 	}
 });
