273c4fe6bb1456fb87571092797e60abd7cfdff2
Twi <git@twiddau.de>
2016-11-13 20:23:48 +0100

    Add additional zoom levels and started hacking a possibility to move polygons

--- client/templates/pages/map.js
+++ client/templates/pages/map.js
@@ -13,76 +13,107 @@ Meteor.subscribe('areas');
 Template.mapPage.rendered = function() {
 	$('#map').css('height', window.innerHeight - 82 - 45);
 
-	L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images/';
+	L.Edit.PolyVerticesEdit.prototype['__initMarkers'] = L.Edit.PolyVerticesEdit.prototype['_initMarkers'];
+	L.Edit.PolyVerticesEdit.prototype['__onMarkerDrag'] = L.Edit.PolyVerticesEdit.prototype['_onMarkerDrag'];
 
-	L.LabelOverlay = L.Class.extend({
-		initialize: function (latLng, label, options) {
-			this._latlng = latLng;
-			this._label = label;
-			L.Util.setOptions(this, options);
+	L.Edit.PolyVerticesEdit.include({
+
+		_initMarkers: function() {
+			L.Edit.PolyVerticesEdit.prototype.__initMarkers.call(this);
+			L.Edit.PolyVerticesEdit.prototype._createMoveMarker.call(this);
 		},
-		options: {
-			offset: new L.Point(0, 2)
+
+		_createMoveMarker: function () {
+			var center = this._poly.getCenter();
+			this._moveMarker = this._createMarker(center, this.options.moveIcon);
 		},
-		onAdd: function (map) {
-			this._map = map;
-			if (!this._container) {
-				this._initLayout();
+
+		_onMarkerDrag: function (e) {
+			var marker = e.target,
+				latlng = marker.getLatLng();
+
+			if (marker === this._moveMarker) {
+				this._move(latlng);
+				this._poly.redraw();
+				this._poly.fire('editdrag');
+			} else {
+				this.__onMarkerDrag(e);
 			}
-			map.getPanes().overlayPane.appendChild(this._container);
-			this._container.innerHTML = this._label;
-			map.on('viewreset', this._reset, this);
-			this._reset();
+
 		},
-		onRemove: function (map) {
-			map.getPanes().overlayPane.removeChild(this._container);
-			map.off('viewreset', this._reset, this);
+
+		_move: function (newCenter) {
+			var latlngs = this._poly.getLatLngs(),
+				center = this._poly.getCenter(),
+				offset, newLatLngs = [];
+
+			// Offset the latlngs to the new center
+			for (var j = 0, l2 = latlngs.length; j < l2; j++ ) {
+				var polyLatLng = latlngs[j];
+				var newPolyLatLngs = [];
+				for (var i = 0, l = polyLatLng.length; i < l; i++) {
+					offset = [polyLatLng[i].lat - center.lat, polyLatLng[i].lng - center.lng];
+					newPolyLatLngs.push([newCenter.lat + offset[0], newCenter.lng + offset[1]]);
+				}
+				newLatLngs.push(newPolyLatLngs);
+			}
+
+			this._poly.setLatLngs(newLatLngs);
+
+			this._markerGroup.eachLayer(function (marker) {
+				if (marker != this._moveMarker) {
+					marker.remove();
+				}
+			}, this);
+			this.__initMarkers();
+			this._map.fire(L.Draw.Event.EDITMOVE, {layer: this._poly});
 		},
-		_reset: function () {
-			console.log(this._container.style.width);
-			var pos = this._map.latLngToLayerPoint(this._latlng);
-			var op = new L.Point(pos.x + this.options.offset.x, pos.y - this.options.offset.y);
-			L.DomUtil.setPosition(this._container, op);
-		},
-		_initLayout: function () {
-			this._container = L.DomUtil.create('div', 'leaflet-label-overlay');
-		}
+
 	});
 
-
+	L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images/';
 
 	var map = L.map('map', {
 		crs: L.CRS.Simple,
-		minZoom: 18,
-		maxZoom: 20,
+		minZoom: 19,
+		maxZoom: 22,
 	});
 
 	var areaLayers = new L.FeatureGroup();
 	map.addLayer(areaLayers);
 
-	var drawControl = new L.Control.Draw({
-		draw: {
 
-			polygon: {
-				allowIntersection: false,
-				showArea: true,
-			},
-
-			rect: {
-				shapeOptions: {
-					color: 'green'
+	if (Roles.userIsInRole(Meteor.userId(), ['areas-editor'])) {
+		var drawControl = new L.Control.Draw({
+			draw: {
+				polygon: {
+					allowIntersection: false,
+					showArea: true,
 				},
-			},
-			polyline: false,
-			circle: false
-		},
 
-		edit: {
-			remove: true,
-			featureGroup: areaLayers
-		}
-	});
-	map.addControl(drawControl);
+				rect: {
+					shapeOptions: {
+						color: 'green'
+					},
+				},
+				polyline: false,
+				polyline: false,
+				circle: false,
+				marker: false
+			},
+
+			edit: {
+				remove: true,
+				featureGroup: areaLayers,
+				edit: {
+					selectedPathOptions: {
+						maintainColor: true
+					}
+				}
+			}
+		});
+		map.addControl(drawControl);
+	}
 
 // 1378x513 is the image
 	var northEast = map.unproject(L.point(0,513), 20);
@@ -91,8 +122,9 @@ Template.mapPage.rendered = function() {
 	var maxLatLngBounds = L.latLngBounds(southWest, northEast);
 
 	L.tileLayer('map-tiles/{z}/map_tile_{x}_{y}.png', {
-		minZoom: 18,
-		maxZoom: 20,
+		minZoom: 19,
+		maxZoom: 22,
+		maxNativeZoom: 20,
 		bounds: maxLatLngBounds
 	}).addTo(map);
 
@@ -128,7 +160,6 @@ Template.mapPage.rendered = function() {
 
 		for (key in e.layers._layers) {
 			val = e.layers._layers[key];
-			console.log(key);
 
 			Areas.update(key, {$set: {
 				latLngs: val._latlngs[0]
@@ -149,14 +180,11 @@ Template.mapPage.rendered = function() {
 
 			var area = L.GeometryUtil.geodesicArea(newDocument.latLngs);
 			polygon.bindTooltip(newDocument.title + "<br />" + L.GeometryUtil.readableArea(area, true), {permanent: true, direction:"center", className: "no-tooltip"}).openTooltip();
-
 		},
 
 		removed: function (oldDocument) {
 			layers = areaLayers._layers;
 			var key, val;
-			console.log(oldDocument);
-
 			for (key in layers) {
 				val = layers[key];
 
@@ -177,8 +205,6 @@ Template.mapPage.rendered = function() {
 					areaLayers.removeLayer(val);
 				}
 			}
-
-			console.log(newDocument);
 
 			polygon = L.polygon(newDocument.latLngs, { color: newDocument.color } );
 			polygon._leaflet_id = newDocument._id;
